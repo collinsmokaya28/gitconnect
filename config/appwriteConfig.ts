c7a61bcff16c9f7f1bd04f5a6ab00d3a757d1dff
+ <+>
2024-09-25 15:05:14 +0300

    Create AppwriteContext.tsx and update appwriteConfig.tsx

--- config/appwriteConfig.ts
+++ config/appwriteConfig.ts
@@ -1,7 +1,7 @@
 import { Client, Account, Databases } from 'appwrite';
 
 const client = new Client();
-client.setEndpoint('YOUR_APPWRITE_ENDPOINT').setProject('YOUR_PROJECT_ID');
+client.setEndpoint('https://cloud.appwrite.io/v1').setProject('66f3faf700240551a191');
 
 export const account = new Account(client);
 export const database = new Databases(client);
